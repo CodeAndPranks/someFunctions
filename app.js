032a518e679cb41251e97de9b5c9d62e4ada72a0
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2025-03-13 17:52:46 +0100

    Update app.js
    Updated,and removed the Crash function to own repo.

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-  //Function messUp;
- //Rotate script-tag you want,or all with astric(*)
-let R = 0;//intilyze variable (R).
-
-// Define the variables used in the animation
-let x1 = 1, x2 = 2, x3 = 3, x4 = 100, x5 = 150;
-let y1 = 1, y2 = 2, y3 = 3, y4 = 100, y5 = 150;
-
-// messUp() is called by button click here:
-function messUp() {
-    // Get all elements (images or others)
-    let DI = document.getElementsByTagName("*");//Expriment with ,h1,h2,img,/*. 
-    let DIL = DI.length;
-
-    //animate the elements
-    function A() {
-        for (let i = 0; i < DIL; i++) { 
-            let DIS = DI[i].style; 
-            DIS.position = 'absolute'; 
-            DIS.left = (Math.sin(R * x1 + i * x2 + x3) * x4 + x5) + "px"; 
-            DIS.top = (Math.cos(R * y1 + i * y2 + y3) * y4 + y5) + "px";
-        }
-        R++;
-    }
-
-    // Start the animation with a 200 ms interval//Try differens speed
-    let intervalID = setInterval(A, 200); 
-
-    // Stop the animation after 5 seconds
-    setTimeout(function() {
-        clearInterval(intervalID); // Stop animation,
-        location.reload();         
-    }, 5000);                      //rotate time.
-}   
  //Function: Mirror & reset page function.
      function mirrorPage () {
             document.body.style.transform = "scaleX(-1)";
@@ -49,7 +15,47 @@ function messUp() {
                 location.reload(); 
             }, 2000);
         }
-        
+ 
+//Function messUp :
+let radius = 100; // Set a fixed radius for the spinning effect
+ 
+// Initialize  variable R
+let R = 0;
+
+// Define variables used in  animation
+let x1 = 1, x2 = 2, x3 = 3, x4 = 100, x5 = 150;
+let y1 = 1, y2 = 2, y3 = 3, y4 = 100, y5 = 150;
+
+//  call function here: [button click]
+function messUp () {
+     //Rotate any script-tag's you like ,or all tag-name  /*
+  
+      let DI = document.querySelectorAll( '*');  
+    
+
+    let DIL = DI.length;
+
+    //animate the elements
+    function A() {
+        for (let i = 0; i < DIL; i++) { 
+            let DIS = DI[i].style; 
+            DIS.position = 'absolute'; 
+            DIS.left = (Math.sin(R * x1 + i * x2 + x3) * x4 + x5) + "px"; 
+            DIS.top = (Math.cos(R * y1 + i * y2 + y3) * y4 + y5) + "px";
+        }
+        R++;
+    }
+
+    // Start the animation with a 200 ms interval//Try differens speed
+    let intervalID = setInterval(A, 200); 
+
+    // Stop the animation after 2 seconds
+    setTimeout(function() {
+        clearInterval(intervalID); // Stop the animation(4 sec)
+        location.reload();         //reload after 4 sec animation(spinn)
+    }, 4000);                      //Costumice 
+}
+
  //Function:Spinn page faste-and-faster 
  function spinnPage () {
 document.documentElement.style.transitionDuration="5s";
@@ -58,7 +64,7 @@ document.documentElement.style.transform="rotate(3600deg)";
   }                
     
 //Function:Turn smootly 180dgs to rigth.Images change,and turnRigth pop up("resetImg").
-     function turnLeft() {
+     function turnRight() {
             // Turn Left transformation
             ['', '-ms-', '-webkit-', '-o-', '-moz-'].forEach(function(prefix) {
                 document.body.style[prefix + 'transition'] = 'transform 4s';
@@ -116,42 +122,55 @@ document.documentElement.style.transform="rotate(3600deg)";
          }
   
  //Function:Running-warning text,then after 2 secund empty ugly grey page.
-function go_empty () {      //change a few key variables when txt start run.
-  alert("Was that smart ? \n When finish just reload for start-page:)")
-  document.body.style.backgroundColor="black"
-  h1.innerHTML="EMPTY HARD DISK"
-  h1.style.color="white"
-  img1.style.display="none"
-  img2.style.display="none"   
-  buttons.style.display="none"  
-  h3.style.display="none"
-//Set up needs & speeds for running text.    
-    let timePerLetter = 40,
-    text = document.createTextNode('');
+   function go_empty() { // Change a few key variables when txt starts running.
 
-    document.getElementById("text").appendChild(text);
-    document.getElementById("text").style.color="white"
+    alert("Was that smart? \nWhen finished, just reload for the start page :)");
+    document.body.style.backgroundColor = "black";
+    h1.innerHTML = "EMPTY HARD DISK";
+    h1.style.color = "white";
+    img1.style.display = "none";
+    img2.style.display = "none";   
+    btns.style.display = "none";  
+    h3.style.display = "none";
+    // p.style.display = "none";
 
-function printOut(str) {
-   var i = 0;
-   (function main() {
-    var char = str[i++];
-    text.nodeValue += char;
-if(i < str.length)
-   setTimeout(main, timePerLetter);
- else 
-   setTimeout(function() { //chang background again & remove runner-text 2 secound after it finish.
-     text.nodeValue = ""
-     h1.innerHTML = ""
-     document.body.style.backgroundColor="grey"
-  }, 2000);  
-  })();
+    // Set up needs & speeds for running text.    
+    let timePerLetter = 40;
+    let textContainer = document.getElementById("text");
+    textContainer.innerHTML = ""; // Clear previous text
+    textContainer.style.color = "white";
+
+    function printOut(str) {
+        var i = 0;
+        (function main() {
+            var char = str[i++];
+            // Check for newline character and replace it with <br>
+            if (char === '\n') {
+                textContainer.innerHTML += "<br>"; // Add a line break in HTML
+            } else {
+                textContainer.innerHTML += char; // Add the character to the text
+            }
+
+            if (i < str.length) {
+                setTimeout(main, timePerLetter);
+            } else {
+                setTimeout(function() { //  Timeout give user time to read runner-text before txt disaperas and screen turns ugly grey.
+                    textContainer.innerHTML = "";
+                    h1.innerHTML = "";
+                    document.body.style.backgroundColor = "grey";
+                }, 5000);  
+            }
+        })();
+    }
+
+    // Message with newlines
+    const message = "The hard disk is empty.\nNo files are possible to recover.\nPlease install an OS before you can use this unit.\nAll copyrights reserved by:\n Fed.11/99-ADv/SACR.USA Dist. of Alabama 047/.HP International, Sacramento, California USA.";
+    printOut(message);
 }
-printOut("...The hardisk is empty.Please install a OS before you can use this unit.All Coyrihgts reserved by: Fed.11/99-ADv/SACR.USA Dist.of.Alabm 047/.HP Internactional,Sacramento,California USA.")
-}
-      
+
+
 //Function Freez is tested on Andriod phone & tablets from version 8 to 16.[11/03-2025] 
 //'wiew' try open a 2'nd script,a infinity-loop,while storages fills up and 'wiew's image freez unit..
     function freeze () { 
- window.location.href="https://codeandpranks.github.io/wiew";
+alert("For safety reasons moved to:\n https://codeandpranks.github.io/wiew");
     }
